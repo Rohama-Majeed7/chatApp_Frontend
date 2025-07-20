527a1fd79a260362032e2cc07167ea23653ef1a7
Rohama Majeed <rohamamajeed3@gmail.com>
2025-07-20 19:29:50 +0500

    h

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-app-backend-acio.vercel.app",
+  baseURL: "https://chat-app-backend-acio.vercel.app/api",
   withCredentials: true, // This is important for sending cookies
 });
